b799be05ccbc89966d646263f76bbe05a72dd03d
Bryan Enrique Gonzalez Zambrano <63970786+BryanGonzalezDeveloper@users.noreply.github.com>
2023-07-28 01:54:19 -0700

    login funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,13 @@ import { StatusTaskService } from './services/status-task.service';
 import { JwtInterceptor } from './services/jwt.interceptor';
 import { ToastNoAnimationModule, ToastrModule, provideToastr } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'task', component: TaskComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige a '/login' por defecto
+  { path: '**', component: LoginComponent }, // Ruta comodín para manejar rutas no encontradas
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,8 +31,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    ToastNoAnimationModule.forRoot()
+    ToastNoAnimationModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [LoginService,TaskService,
     StatusTaskService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { SingInRequest } from 'src/app/models/SingIn';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +12,30 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup=new FormGroup({
+    UserName: new FormControl('', [Validators.required]),
+    Password: new FormControl('', [Validators.required])
+  });
+
+  constructor(private loginService:LoginService, private route:Router, private toast:ToastrService) { }
+
+
+  isInvalid(controlName: string) {
+    const control = this.loginForm.get(controlName);
+    return control?.invalid && (control.dirty || control.touched);
+  }
+
+  onSubmit() {
+    this.toast.info('Verificando credenciales...', 'Iniciando sesión', {timeOut: 1000});
+    if (this.loginForm.valid) {
+      const body = this.loginForm.value as SingInRequest;
+      this.loginService.SingIn(body).subscribe(response=>{
+        localStorage.setItem('Token',response.Token);
+        localStorage.setItem('UserId',response.UserId.toString());
+        this.route.navigate(['/task']);
+      },err=>{
+        this.toast.error(err.error.Message,'Error', {timeOut: 2500});
+      });
+    }
+  }
 }
